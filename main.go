bbc45304319df9ef5c0a7c3db798a84028199073
Xiaoyang Tan <tanx@uber.com>
2019-12-23 22:18:17 -0800

    test new version

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
- "fmt"
- "github.com/xytan0056/depwithgen/api"
+	"fmt"
+	"github.com/xytan0056/depwithgen/api"
 )
 
+///dsfasdfasf
 func main() {
 	api.API()
 	fmt.Println("I have gen")
